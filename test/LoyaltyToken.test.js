0e11bb98a1d2405937fa1449cabe7f80c542a43c
David Phan <davidphan33@gmail.com>
2018-08-31 11:16:45 +0100

    LoyaltyToken
    Added bad path token.

--- test/LoyaltyToken.test.js
+++ test/LoyaltyToken.test.js
@@ -1,5 +1,7 @@
 var LoyaltyToken = artifacts.require("./LoyaltyToken.sol");
 
+
+var {expectThrow} = require('./helpers/expectThrow.js');
 contract('LoyaltyToken', async (accounts) => {
 
     owner = accounts[0]
@@ -23,4 +25,15 @@ contract('LoyaltyToken', async (accounts) => {
         receipt = await this.loyaltyTokenInstance.transfer.sendTransaction(someone, 100, {from: owner});
         assert.equal(await this.loyaltyTokenInstance.balanceOf.call(someone), 100);
     });
+
+    it("should not be able to send more tokens that he owns", async () => {
+        receipt = await this.loyaltyTokenInstance.transfer.sendTransaction(someone, 100, {from: owner});
+        assert.equal(await this.loyaltyTokenInstance.balanceOf.call(someone), 100);
+        
+
+        // someone makes a transaction greater than his balance
+        receipt = await expectThrow(this.loyaltyTokenInstance.transfer.sendTransaction(accounts[2], 101, {from: someone}));
+
+        assert.equal(await this.loyaltyTokenInstance.balanceOf.call(someone), 100);        
+    })
 });
